59fe039222a38d0290e99f41a33d13d0826d4e2d
cnt52845 <132798980+cnt52845@users.noreply.github.com>
2023-05-07 23:55:41 +0000

    Refactor

--- fizzbuzz.hpp
+++ fizzbuzz.hpp
@@ -1,18 +1,23 @@
 #ifndef FIZZBUZZ_HPP_
 #define FIZZBUZZ_HPP_
 
+std::string checkFizz(int number) {
+  return (0 == number % 3) ? "Fizz" : "";
+}
+
+std::string checkBuzz(int number) {
+  return (0 == number % 5) ? "Buzz" : "";
+}
+
+std::string checkResult(int number, const std::string& result) {
+  return (result.empty()) ? std::to_string(number) : result;
+}
+
 std::string fizzbuzz(int number) {
   std::string result;
-  if (0 == number % 3) {
-    result += "Fizz";
-  }
-  if (0 == number % 5) {
-    result += "Buzz";
-  }
-  if (result.empty()) {
-    return std::to_string(number);  
-  }
-  return result;
+  result += checkFizz(number);
+  result += checkBuzz(number);
+  return checkResult(number, result);
 }
 
 #endif

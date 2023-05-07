640f128785c080f8fb68b861817ae19b7dbb1e95
Replit user <>
2023-05-07 04:20:41 +0000

    TEST(fizzbuzz, OneReturnsOne)

---
+++ fizzbuzz.hpp
@@ -0,0 +1,8 @@
+#ifndef FIZZBUZZ_HPP_
+#define FIZZBUZZ_HPP_
+
+std::string fizzbuzz(int number) {
+  return "1";  
+}
+
+#endif
--- test_something.cpp
+++ test_something.cpp
@@ -2,9 +2,8 @@
 
 #include <gmock/gmock.h>
 
-TEST(Something, SomethingPass)
-{
-    std::string value{"foo"};
-    EXPECT_TRUE(true);
-    EXPECT_EQ(value, "foo");
+#include "fizzbuzz.hpp"
+
+TEST(fizzbuzz, OneReturnsOne) {
+  EXPECT_EQ("1", fizzbuzz(1));
 }

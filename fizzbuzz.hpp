032dc2074ea2cb1cfffb75d09a48b2bd541e76b6
cnt52845 <132798980+cnt52845@users.noreply.github.com>
2023-05-08 02:07:09 +0000

    Move implementation into class

--- fizzbuzz.hpp
+++ fizzbuzz.hpp
@@ -1,34 +1,31 @@
 #ifndef FIZZBUZZ_HPP_
 #define FIZZBUZZ_HPP_
 
-std::string checkFizz(int number) {
-  return (0 == number % 3) ? "Fizz" : "";
-}
-
-std::string checkBuzz(int number) {
-  return (0 == number % 5) ? "Buzz" : "";
-}
-
-std::string checkFuzz(int number) {
-  return (0 == number % 7) ? "Fuzz" : "";
-}
-
-std::string checkResult(int number, const std::string& result) {
-  return (result.empty()) ? std::to_string(number) : result;
-}
-
-std::string fizzbuzz(int number) {
-  std::string result;
-  result += checkFizz(number);
-  result += checkBuzz(number);
-  result += checkFuzz(number);
-  return checkResult(number, result);
-}
-
 class FizzBuzz {
  public:
   std::string operator()(int number) {
-    return fizzbuzz(number);
+    std::string result;
+    result += checkFizz(number);
+    result += checkBuzz(number);
+    result += checkFuzz(number);
+    return checkResult(number, result);
+  }
+
+ private:
+  std::string checkFizz(int number) {
+    return (0 == number % 3) ? "Fizz" : "";
+  }
+
+  std::string checkBuzz(int number) {
+    return (0 == number % 5) ? "Buzz" : "";
+  }
+
+  std::string checkFuzz(int number) {
+    return (0 == number % 7) ? "Fuzz" : "";
+  }
+
+  std::string checkResult(int number, const std::string& result) {
+    return (result.empty()) ? std::to_string(number) : result;
   }
 };
 
